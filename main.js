015c8bdda34581bc3110158031d14c320335ed45
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-29 08:22:03 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let cont=0;
-while (cont<=10) {
-    let n1=Number(prompt("Escribe el primer numero: "))
-	let n2=Number(prompt("Escribe el segundo numero: "))
-	let sum=n1+n2
-	alert("El resultado de la suma es " +sum)
-	let rest=n1-n2
-	alert("El resultado de la resta es " +rest)
-	let mult=n1*n2
-	alert("El resultado de la multiplicacion es " +mult)
-	let div=n1/n2
-	alert("El resultado de la division es " +div)
-	cont+=1
+let cont=1;
+while(cont<=5){
+    let nombre=prompt("Escribe el nombre del estudiante : ");
+    let apellido=prompt("Escribe el apellido del estudiante: ");
+    let edad=Number(prompt("Escribe la edad del estudiante: "));
+
+    let nota1=Number(prompt("Escribe la primera nota del estudiante: "));
+    let nota2=Number(prompt("Escribe la segunda nota del estudiante: "));
+
+    let promedio=(nota1+nota2)/2;
+    alert(" El estudiante " + nombre + apellido + " su edad es de " + edad + " tiene una nota promedio  de " + promedio);
+    cont+=1
 }
+
+
 alert("Fin del Programa")
