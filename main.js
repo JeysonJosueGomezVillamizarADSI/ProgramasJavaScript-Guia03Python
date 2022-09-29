0dd9b4d4fd0757211e5075e0a39af1bca2420eb8
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-29 08:42:08 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-let dama=0;
-let caballero=7;
+let noTienenDerecho=0;
+let cont=0;
 
-while (dama+caballero<=42) {
-    dama+=1;
-    caballero+=1;
-    
+while (cont<=40) {
+    let nt1 = Number(prompt("Digite la primera nota : "));
+
+    let nt2 = Number(prompt("Digite la segunda nota : "));
+
+    let nt3 = Number(prompt("Digite la tercera nota : "));
+
+    let nt4 = Number(prompt("Digita la cuarta nota : "));
+
+    let nt5 = Number(prompt("Digite la quinta nota : "));
+
+    let pro = (nt1 + nt2 + nt3 + nt4 + nt5) / 5;
+    cont += 1;
+
+    if (pro<=3) {
+        noTienenDerecho+=1;
+    }
+    alert("La cantidad de alumnos que no tienen derecho al examen de nivelacion son : "+noTienenDerecho)
 }
-alert("Las damas que habia en el baile eran " +dama)
-
 
 alert("Fin del Programa")
