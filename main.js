9ab571418907ed5c6a89b23b0f9b82a8cfe16fcd
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-29 08:27:57 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-let cont=1;
-while(cont<=5){
-    let nombre=prompt("Escribe el nombre del estudiante : ");
-    let apellido=prompt("Escribe el apellido del estudiante: ");
-    let edad=Number(prompt("Escribe la edad del estudiante: "));
+let dama=0;
+let caballero=7;
 
-    let nota1=Number(prompt("Escribe la primera nota del estudiante: "));
-    let nota2=Number(prompt("Escribe la segunda nota del estudiante: "));
-
-    let promedio=(nota1+nota2)/2;
-    alert(" El estudiante " + nombre + apellido + " su edad es de " + edad + " tiene una nota promedio  de " + promedio);
-    cont+=1
+while (dama+caballero<=42) {
+    dama+=1;
+    caballero+=1;
+    
 }
+alert("Las damas que habia en el baile eran " +dama)
 
 
 alert("Fin del Programa")
