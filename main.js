c5157294662eb2ffce526dbbe84c0d909726b261
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-29 15:41:22 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,170 +1,39 @@
-let cont = 1;
-let opcionA = 0;
-let opcionB = 0;
-let opcionC = 0;
+let vehiculo=4;
+let turismo=0;
+let pert=0;
+let autobus=0;
+let camioneta=0;
+let motocicleta=0;
+let cont=1;
 
-let cantidad = Number(prompt("Digite la cantidad de alumnos que van a votar : "))
-while (cont <= cantidad) {
-    let representante = prompt("Eliga el representante por el cual desea votat: (A,B o C : )")
-    cont += 1;
-    if (representante == "A") {
-        opcionA += 1;
-    } else if (representante == "B") {
-        opcionB += 1;
-    } else {
-        opcionC += 1;
-    }
+while (cont<=vehiculo) {
+    let tipoVehiculo=prompt("Ingrese el tipo de vehiculo que esta pasando (turismo, autobus, camion o motocicleta.: )");
+    cont+=1
 
-    let totalA = (opcionA / cantidad) * 100;
-    let totalB = (opcionB / cantidad) * 100;
-    let totalC = (opcionC / cantidad) * 100;
-    if (totalA > totalB && totalA > totalC) {
+    if (tipoVehiculo=='turismo'){
 
-        alert('El porcentaje de la votacion del representante A es' + totalA)
-        alert('El porcentaje de la votacion del representante B es' + totalB)
-        alert('El porcentaje de la votacion del representante C es' + totalC)
-    } else if (totalB > totalA && totalB > totalC) {
+        let per=int(input('Cantidad de personas: '))
+        turismo+=1
+        pert=per+pert}
 
-        alert('El porcentaje de la votacion del representante A es' + totalA)
-        alert('El porcentaje de la votacion del representante B es' + totalB)
-        alert('El porcentaje de la votacion del representante C es' + totalC)
-    }
+    if(tipoVehiculo=='autobus'){
 
-    if (totalC > totalA && totalC > totalB) {
+        autobus+=1}
 
-        alert('El porcentaje de la votacion del representante A es' + totalA)
-        alert('El porcentaje de la votacion del representante B es' + totalB)
-        alert('El porcentaje de la votacion del representante C es' + totalC)
-    }
-    if (totalB == totalA && totalB == totalC) {
+    if(tipoVehiculo=='camion'){
 
-        print('SE CANCELAN ELECCIONES DE REPRESENTANTE')
-    }
+        camioneta+=1}
+    
+    else{
 
-    else if(totalA >= totalB && totalB > totalC){
-
-        cont = 1
-        opcionA = 0
-        opcionB = 0
-
-        alert('HAY UN EMPATE ENTRE A Y B')
-
-        cantidad = Number(prompt('Digite la cantidad de alumnos que van a votar: '))
-
-        while (cont <= cantidad) {
-
-            cont += 1
-
-            representante = Number(prompt('Digite el representante por el que va a votar (A o B): ')
-
-        if (rep == 'A') {
-                a += 1
-            }
-            else { b += 1 }
-        }
-
-        totalA = (opcionA / cantidad) * 100
-        totalB = (opcionB / cantidad) * 100
-
-        if (tota >= 51) {
-
-            alert('El ganador de las votaciones es A con un' + totalA)
-        }
-
-        if (totalB >= 51) {
-
-            alert('El ganador de las votaciones es B con un' + totalB)
-        }
-
-        else {
-
-            alert('DOBLE EMPATE ENTRE A Y B')
-        }
-
-        if (totalA >= totalC && totc > totb) {
-
-            con = 1
-            a = 0
-            c = 0
-
-            alert('HAY UN EMPATE ENTRE A Y C')
-        }
-
-        cantidad = Number(prompt('Digite la cantidad de alumnos que van a votar: '))
-
-        while (cont <= cantidad{
-
-            cont += 1
-
-            representante = prompt('Digite el representante por el que va a votar (A o C): ')
-
-            if (rep == 'A') {
-                a += 1
-            }
-
-            else { c += 1 }
-        }
-
-        totalA = (opcionA / cantidad) * 100
-        totalC = (opcionC / cantidad) * 100
-
-        if (tota >= 51) {
-            alert('El ganador de las votaciones es A con un' + totalA)
-        }
-
-        else if (totc >= 51) {
-            alert('El ganador de las votaciones es C con un' + totalC)
-        }
+        motocicleta+=1}
+}
+alert('Cantidad de autobuses',aut)
+alert('Cantidad de camiones',cam)
+alert('Cantidad de motocicletas',mot)
+alert('Cantidad de turismo',tur)
+alert('Cantidad de personas en vehiculos de turismo',pert)
 
 
-
-        else {
-
-            print('DOBLE EMPATE ENTRE A Y C')
-        }
-    }
-
-else {
-            cont = 1
-            opcionB = 0
-            opcionC = 0
-
-            alert('HAY UN EMPATE ENTRE B Y C')
-
-            cantidad = Number(prompt('Digite la cantidad de alumnos que van a votar: '))
-
-            while (con <= can) {
-
-                cont += 1
-
-                representante = prompt('Digite el representante por el que va a votar (B o C): ')
-
-                if (representante == 'B') {
-                    opcionB += 1
-                }
-
-                else {
-                    opcionC += 1
-                }
-            }
-            totalB = (opcionB / cantidad) * 100
-            totalC = (opcionC / cantidad) * 100
-
-            if (totalB >= 51) {
-
-                alert('El ganador de las votaciones es B con un' + totalB)
-            }
-
-            else if (totalC >= 51) {
-
-                print('El ganador de las votaciones es C con un' + totalC)
-            }
-
-            else {
-
-                print('DOBLE EMPATE ENTRE B Y C')
-            }
-        }
-    }
 
 alert("Programa Finalizado")
