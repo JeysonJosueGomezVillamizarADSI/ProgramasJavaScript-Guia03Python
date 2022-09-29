d65cd94af5d316e1d875650ff2d16c5999b9c28a
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-29 09:43:28 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-let segundos=0;
-let avion=222222;
-let misil=222222;
-let aceleracionAvion=20;
-let aceleracionMisil=10;
+let numTrabajadores=Number(prompt("Ingresa el numero de trabajadores : "));
+let cont=1;
+let comision=0;
 
-while (aceleracionAvion-aceleracionMisil<=10000) {
-    aceleracionAvion+=20+aceleracionAvion;
-    aceleracionMisil+=10+aceleracionMisil;
-    segundos+=1
+while (cont<=numTrabajadores) {
+    let sueldo=Number(prompt("Ingrese el sueldo del trabajador : "));
+    let venta1= Number(prompt("Ingrese la primera venta : "));
+    let venta2=Number(prompt("Ingrese la segunda venta : "));
+    let venta3=Number(prompt("Ingrese la tercera venta : "));
 
-    alert ('segundo ' + segundos)
-    alert('distancia del avion ' +aceleracionAvion)
-    alert('distancia del misil ' +aceleracionMisil)
+    comision=(venta1+venta2+venta3)*0.10;
+
+    alert("El trabajador tiene un sueldo mensual de " + sueldo + "con una comision mensual de " + comision );
+    alert("El sueldo total mas la comision es de " + (sueldo+comision));
 }
 
 alert("Programa Finalizado");
